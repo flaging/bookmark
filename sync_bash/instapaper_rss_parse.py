e81bbd4f9d54f17c031dff7fd64ed188bcddb951
flaging <liyupeng341@163.com>
2020-12-16 23:02:23 +0800

    Update instapaper_rss_parse.py

--- sync_bash/instapaper_rss_parse.py
+++ sync_bash/instapaper_rss_parse.py
@@ -4,34 +4,69 @@ import urllib.request
 import sys
 import os
 import time
-def main():
-  data = feedparser.parse("https://www.instapaper.com/rss/6354015/oRVYvTSMb5bHxEFJn9qvi4xUUEw")
-  print(data.feed.title)
-  localtime = time.localtime(time.time())
-  file_name = str(localtime.tm_year)+"/"+str(localtime.tm_mon)+str(localtime.tm_mday)+".md"
-  readme = open("README.md","a")
-  readme.writelines("\n\n["+str(localtime.tm_year)+"-"+str(localtime.tm_mon)+"-"+str(localtime.tm_mday)+"]("+file_name+")")
-  readme.close()
-  file=open(file_name,"a")
-  file.write("\n# "+str(localtime.tm_year)+"-"+str(localtime.tm_mon)+"-"+str(localtime.tm_mday))
+import sqlite3
+def parse_url(str1,file):
+  data = feedparser.parse(str1)
+  if os.path.exists('test.db'):
+    conn = sqlite3.connect('test.db')
+    cur = conn.cursor()
+  else:
+    conn = sqlite3.connect('test.db')
+    cur = conn.cursor()
+    sql_text_1 = '''CREATE TABLE scores 
+              ( ID NUMBER,
+                URL TEXT);''' 
+    cur.execute(sql_text_1)
+  
   for entry in data.entries:
     url = entry.link
-    print(url)
-    headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}
-    req=urllib.request.Request(url, headers=headers)
-    try:
-      resp=urllib.request.urlopen(req)
-    except (urllib.HTTPError,e):
-      print('Error code:', e.code)
-    except (urlllib.URLError, e):
-      print( 'We failed to reache a server.')
-      print('Reason:', e.reason)
-    else:
-      data=resp.read().decode('utf-8')
-      soup = BeautifulSoup(data,'lxml')
-      file.writelines("\n\n### [" +str(soup.title.get_text())+"]("+url+")")
+    exe2= "SELECT * FROM scores WHERE URL == " + '"'+url+'"'
+    print(exe2)
+    result = cur.execute(exe2)
+    count = 0
+    for i in result:
+      print(i)
+      count = count + 1
+    # exe3 = "select * from scores"
+    # result1 = cur.execute(exe3)
+    # for i in result1:
+    #   print(i)
+    print(count)
+    if count == 0:
+      exe1 = 'INSERT INTO scores VALUES(1,"'+url+'")'
+      print(exe1)
+      cur.execute(exe1)
+      conn.commit()
+      headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}
+      req=urllib.request.Request(url, headers=headers)
+      try:
+        resp=urllib.request.urlopen(req)
+      except (urllib.error.HTTPError,e):
+        print('Error code:', e.code)
+      except (urlllib.error.URLError, e):
+        print( 'We failed to reache a server.')
+        print('Reason:', e.reason)
+      else:
+        data=resp.read().decode('utf-8','ignore')
+        soup = BeautifulSoup(data,'lxml')
+        file.writelines("\n\n### [" +str(soup.title.get_text())+"]("+url+")")
 
+
+def main():
+  localtime = time.localtime(time.time())
+  file_name = str(localtime.tm_year)+"/"+str(localtime.tm_mon)+str(localtime.tm_mday)+".md"
+  is_exists = os.path.exists(file_name)
+  file=open(file_name,"a")
+  if not is_exists:
+    file.write("\n# "+str(localtime.tm_year)+"-"+str(localtime.tm_mon)+"-"+str(localtime.tm_mday))
+  url_list = open('list.txt','r').read().splitlines()
+  for url in url_list:
+    parse_url(url, file)
   file.close()
-
+  if not is_exists:
+    readme = open("README.md","a")
+    readme.writelines("\n\n["+str(localtime.tm_year)+"-"+str(localtime.tm_mon)+"-"+str(localtime.tm_mday)+"]("+file_name+")")
+    readme.close()
+  
 if __name__ == "__main__":
   main()
